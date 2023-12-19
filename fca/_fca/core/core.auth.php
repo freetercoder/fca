1549a82916d97532b427f0a2c248858f89333a06
freetercoder <freetercoder@gmail.com>
2023-12-19 11:27:08 +0900

    ext 삭제. member 삭제. member 로직 auth로 통합.

--- fca/_fca/core/core.auth.php
+++ fca/_fca/core/core.auth.php
@@ -29,4 +29,19 @@ class FAuth{
 
         return $bearer;
     }
+
+    public static function member_exist_or_401(){
+        $member_token = FAuth::bearer_or_401();    
+        $member = FDB::first_or_401("member", "member_token", $member_token);
+        return $member;
+    }
+
+    public static function member_owner_or_400($field, $error_message="NOT_OWNER"){
+        $member = self::member_exist_or_401();
+        if ($field !== $member["id"]){
+            FResponse::_400_bad_request($error_message);
+        }
+
+        return $member;
+    }
 }
--- fca/index.php
+++ fca/index.php
@@ -1,6 +1,5 @@
 <?php
 require_once($_SERVER["DOCUMENT_ROOT"] . "/_fca/core/core.php");
-require_once($_SERVER["DOCUMENT_ROOT"] . "/_fca/ext/ext.php");
 
 const FCA = true;
 FImport::php("_fca/user/user.config");
